adb1d02dbf26e419c4348e9e3d4e07cf64702044
L3ws3r <132311932+L3ws3r@users.noreply.github.com>
2025-05-10 21:15:39 -0400

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,13 +1,9 @@
 
-// Basic routing
-document.getElementById("settingsBtn").addEventListener("click", () => {
-    document.getElementById("homeScreen").classList.add("hidden");
-    document.getElementById("gameScreen").classList.add("hidden");
-    document.getElementById("settingsScreen").classList.remove("hidden");
-});
-
-document.getElementById("homeBtn").addEventListener("click", () => {
-    document.getElementById("settingsScreen").classList.add("hidden");
-    document.getElementById("gameScreen").classList.add("hidden");
-    document.getElementById("homeScreen").classList.remove("hidden");
+document.addEventListener("DOMContentLoaded", () => {
+  const settingsBtn = document.getElementById("settingsBtn");
+  if (settingsBtn) {
+    settingsBtn.onclick = () => {
+      window.location.href = "settings.html";
+    };
+  }
 });
