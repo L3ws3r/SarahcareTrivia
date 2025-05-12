e1eeb99dd31cd434212a8f0d354a2b2db79510c6
L3ws3r <132311932+L3ws3r@users.noreply.github.com>
2025-05-11 21:16:13 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.post("/ask", async (req, res) => {
