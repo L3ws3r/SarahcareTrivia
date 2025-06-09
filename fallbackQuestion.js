6eb771c4ee38b65380f9b4b56114932723bcb497
L3ws3r <132311932+L3ws3r@users.noreply.github.com>
2025-06-08 21:26:14 -0400

    Delete fallbackQuestion.js

--- fallbackQuestion.js
+++
@@ -1,7 +0,0 @@
-export default {
-  question: "What color is the sky on a clear, sunny day?",
-  correct: "Blue",
-  distractors: ["Green", "Red", "Purple"],
-  answers: ["Blue", "Green", "Red", "Purple"],
-  funFact: "The sky appears blue because molecules in the air scatter blue light from the sun more than they scatter red light."
-};
